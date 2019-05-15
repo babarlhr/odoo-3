bb59a3521b938392d0de96c54e554fe9f3f064e3
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-05-15 18:13:15 -0500

    sotck update 05

--- hs_stock_extra/models/stock.py
+++ hs_stock_extra/models/stock.py
@@ -1,6 +1,32 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
+class StockDeliveryOrderInherit1(models.Model):
+	_inherit = "stock.picking"
+	hs_display = fields.Boolean("Display Managment", compute="_compute_display_mangment", store=False)
+	hs_managment = fields.Char('Administrador', compute="_compute_managment", store=False)
+
+
+	@api.one
+	@api.depends('name')
+	def _compute_managment(self):
+		if self.env["sale.order"].search_count(["name", "=", self.origin]) > 0:
+			env = self.env["sale.order"].search(["name", "=", self.origin], limit=1)
+			self.hs_managment = env.user_id.name
+		elif self.env["purchase.order"].search_count(["name", "=", self.origin]) > 0:
+			env = self.env["purchase.order"].search(["name", "=", self.origin], limit=1)
+			self.hs_managment = env.user_id.name
+		else:
+			self.hs_managment = ""
+
+
+	@api.one
+	@api.depends('name')
+	def _compute_display_mangment(self):
+		self.hs_display = True
+
+
+"""
 class StockPickingInherit(models.Model):
 	_inherit = "stock.picking"
 	#hs_category = fields.Char(related='picking_type_id.name', string="Operation Type", store=False)
@@ -36,3 +62,4 @@ class StockPickingInherit(models.Model):
 					('done', 'Completado'),
 					('cancel', 'Cancelled'),
 			]
+"""
