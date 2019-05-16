c1351869f014332e4cdc49fdb2294aa8e13cd32d
Luis Dominguez <luis.dominguez19@yahoo.es>
2019-05-16 14:01:30 -0500

    stock update 09

--- hs_stock_extra/models/stock.py
+++ hs_stock_extra/models/stock.py
@@ -10,14 +10,16 @@ class StockDeliveryOrderInherit1(models.Model):
 	@api.one
 	@api.depends('name', 'origin')
 	def _compute_managment(self):
-		if self.env["sale.order"].search_count([("name", "=", self.origin)]) > 0:
+		if self.origin == False:
+			self.hs_managment = False
+		elif self.env["sale.order"].search_count([("name", "=", self.origin)]) > 0:
 			env = self.env["sale.order"].search([("name", "=", self.origin)], limit=1)
 			self.hs_managment = env.user_id.name
 		elif self.env["purchase.order"].search_count([("name", "=", self.origin)]) > 0:
 			env = self.env["purchase.order"].search([("name", "=", self.origin)], limit=1)
 			self.hs_managment = env.user_id.name
 		else:
-			self.hs_managment = ""
+			self.hs_managment = False
 
 
 	@api.one
